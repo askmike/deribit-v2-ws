84cfbaf739e0f68bddfb60e53f9909390221fe23
Mike van Rossum <mike@mikevanrossum.nl>
2019-03-19 18:57:24 +0800

    use async/await example

--- example.js
+++ example.js
@@ -3,7 +3,10 @@ const lib = require('./index');
 const key = 'x';
 const secret = 'y';
 
-lib.init(key, secret);
-lib.request('private/get_position', {instrument_name: 'BTC-PERPETUAL'})
-  .then(resp => console.log(resp))
-  .catch(console.error);
+(async () => {
+
+  lib.init(key, secret);
+  const resp = await lib.request('private/get_position', {instrument_name: 'BTC-PERPETUAL'});
+
+  console.log(resp);
+})()
